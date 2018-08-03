477743f9f07e2c8e2a0a08509b565885ac7a8341
can <mcansevinc@gmail.com>
2018-08-03 14:11:54 +0300

    corrected := as =

--- hello.go
+++ hello.go
@@ -6,11 +6,10 @@ import (
 
 func say(s string, c chan int) {
 	var i int
-	for i := 0; i < 5; i++ {
+	for i = 0; i < 5; i++ {
 		fmt.Println(s, i)
 	}
-	fmt.Println("expected i = 4. real i =", i)
-	// why is i=0? i expect it to be 4.
+	fmt.Println("expected i = 5. real i =", i)
 	c <- i
 }
 
